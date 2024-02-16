dc254af3bf7725948f838bc58c1b6169eef0b342
Chalanka Kodikara <ischalankakodikara@gmail.com>
2024-02-16 10:33:11 +0530

    test

---
+++ src/lib/consts/navigation.jsx
@@ -0,0 +1,55 @@
+import { FaCommentMedical } from "react-icons/fa";
+
+export const DASHBOARD_SIDEBAR_LINKS = [
+  {
+    key: "recipe",
+    label: "Create Recipe",
+    path: "/",
+    icon: <FaCommentMedical />,
+  },
+  // {
+  //   key: "products",
+  //   label: "Products",
+  //   path: "/products",
+  //   icon: <HiOutlineCube />,
+  // },
+  // {
+  //   key: "orders",
+  //   label: "Orders",
+  //   path: "/orders",
+  //   icon: <HiOutlineShoppingCart />,
+  // },
+  // {
+  //   key: "customers",
+  //   label: "Customers",
+  //   path: "/customers",
+  //   icon: <HiOutlineUsers />,
+  // },
+  // {
+  //   key: "transactions",
+  //   label: "Transactions",
+  //   path: "/transactions",
+  //   icon: <HiOutlineDocumentText />,
+  // },
+  // {
+  //   key: "messages",
+  //   label: "Messages",
+  //   path: "/messages",
+  //   icon: <HiOutlineAnnotation />,
+  // },
+];
+
+// export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
+//   {
+//     key: "settings",
+//     label: "Settings",
+//     path: "/settings",
+//     icon: <HiOutlineCog />,
+//   },
+//   {
+//     key: "support",
+//     label: "Help & Support",
+//     path: "/support",
+//     icon: <HiOutlineQuestionMarkCircle />,
+//   },
+// ];
